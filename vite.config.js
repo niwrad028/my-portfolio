922d47fa53fe64a1ab6055e36e2ac4679928f822
darwin <niwrad028.dp@gmail.com>
2024-05-30 21:53:20 +0800

    final commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/my-app/",
+  base: "/my-portfolio/",
   plugins: [react()],
   server: {
     port: 8001,
