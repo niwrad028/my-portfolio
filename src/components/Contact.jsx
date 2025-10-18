126ec345b20239167a91b27af8222f3b633d34cf
niwrad028 <niwrad028.dp@gmail.com>
2025-10-18 22:21:44 +0800

    updated public key

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
-import React, { useRef, useState } from 'react';
-import emailjs from '@emailjs/browser';
-import toast from 'react-hot-toast';
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import toast from "react-hot-toast";
 
 const Contact = () => {
   const [name, setName] = useState("");
@@ -21,38 +21,69 @@ const Contact = () => {
     e.preventDefault();
 
     emailjs
-      .sendForm('service_ap7cpjo', 'template_uevt30b', form.current, {
-        publicKey: '-oceXjhy8swfZYPe_',
+      .sendForm("service_ap7cpjo", "template_uevt30b", form.current, {
+        publicKey: "-r0-4h2ZU4S6JiLLj8_",
       })
       .then(
         () => {
-          toast.success('Email message was sent!', { duration: 3000 });
-          setName("")
-          setEmail("")
-          setMessage("")
-
+          toast.success("Email message was sent!", { duration: 3000 });
+          setName("");
+          setEmail("");
+          setMessage("");
         },
         (error) => {
-          toast.error('FAILED...', error.text);
-        },
+          toast.error("FAILED...", error.text);
+        }
       );
   };
   return (
-    <section id="contact" className="py-10 px-3 text-white flex min-h-screen justify-center items-center">
+    <section
+      id="contact"
+      className="py-10 px-3 text-white flex min-h-screen justify-center items-center"
+    >
       <div className="text-center mt-8">
         <h3 className="text-4xl font-semibold">
-          <span className="text-gray-600">Contact</span> <span className="text-cyan-600">Me</span>
+          <span className="text-gray-600">Contact</span>{" "}
+          <span className="text-cyan-600">Me</span>
         </h3>
         <p className="text-gray-400 my-3 text-lg">Get in touch with me</p>
         <div
           className="mt-5 flex md:flex-row flex-col
          gap-6 max-w-6xl bg-gray-800 md:p-6 p-2 rounded-lg mx-auto"
         >
-          <form ref={form} onSubmit={sendEmail} className="flex flex-col flex-1 gap-5" autoComplete="off">
-            <input type="text" placeholder="Your Name" name="user_name" autoComplete='false' value={name} onChange={(e) => setName(e.target.value)} />
-            <input type="Email" placeholder="Your Email Address" name="user_email" autoComplete='false' value={email} onChange={(e) => setEmail(e.target.value)} />
-            <textarea placeholder="Your Message" rows={7} name="message" autoComplete='false' value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
-            <button className="btn-primary w-fit" type="submit" value="Send">Send Message</button>
+          <form
+            ref={form}
+            onSubmit={sendEmail}
+            className="flex flex-col flex-1 gap-5"
+            autoComplete="off"
+          >
+            <input
+              type="text"
+              placeholder="Your Name"
+              name="user_name"
+              autoComplete="false"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              type="Email"
+              placeholder="Your Email Address"
+              name="user_email"
+              autoComplete="false"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <textarea
+              placeholder="Your Message"
+              rows={7}
+              name="message"
+              autoComplete="false"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
+            <button className="btn-primary w-fit" type="submit" value="Send">
+              Send Message
+            </button>
           </form>
           <div className="flex flex-col md:gap-8 gap-1 pb-3 basis-1/2 justify-start ">
             {contact_info.map((contact, i) => (
